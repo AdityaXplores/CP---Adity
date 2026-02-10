81c2dd34a47a500b0354cf6d30980bfc2553778b
Aditya Xplores <adityalahane65@gmail.com>
2026-02-10 12:26:21 +0530

    Add Template.cpp; include/macro changes
    Adds a full C++ contest template (Template.cpp) with common typedefs, macros, I/O helpers, debug utilities and a main/solve skeleton. Also updates Common Techniques/Distinct in Range Queries.cpp to include <bits/stdc++.h> and adds ff/ss macros (mapped to front.first and front.second) used by the compare function. These changes provide a reusable starter template and small adjustments to the existing query file.

--- Common Techniques/Distinct in Range Queries.cpp
+++ Common Techniques/Distinct in Range Queries.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+#define ff front.first
+#define ss front.second
 bool compare(pair<pair<int, int>, int>& a, pair<pair<int, int>, int>& b){
     return a.ff.ss < b.ff.ss;
 }
